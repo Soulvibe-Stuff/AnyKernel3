abe07485c869a5bdb36aabadae6a966b44c31643
Olga dé claire <olgafadillah@proton.me>
2024-05-05 10:52:34 -0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Paradox kernel by Dreams @soulvibe
+kernel.string=Paradox kernel by Dreams @ soulvibe-team
 do.devicecheck=1
 do.modules=0
 do.systemless=1
