353e3be2618d4c0b689277df689adcdc47d1d0e3
Fitri Rasendriya <fitri.81310740@smkpgriwonoasri.sch.id>
2024-06-05 17:02:52 -0700

    Rebrand to The One Memory
    Signed-off-by: Fitri Rasendriya <fitri.81310740@smkpgriwonoasri.sch.id>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Paradox kernel @soulvibe-team
+kernel.string=TheOneMemory kernel @soulvibe-team
 do.devicecheck=1
 do.modules=0
 do.systemless=1
